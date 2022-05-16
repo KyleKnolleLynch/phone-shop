2940ec6653fba3b2b500744a695f226a6e6dc015
KyleKnolleLynch <kylelynch104@gmail.com>
2022-05-16 15:28:00 -0500

    RTK query - getAllProducts, Product component

--- backend/products.js
+++ backend/products.js
@@ -4,21 +4,21 @@ const products = [
     name: 'iPhone 12 Pro',
     desc: '6.1-inch display',
     price: 999,
-    image: '',
+    image: 'https://res.cloudinary.com/db5zkk8em/image/upload/v1652720418/phoneShop/iphone12pro_ayoufk.jpg',
   },
   {
     id: 2,
     name: 'iPhone 12',
     desc: '5.4-inch display',
     price: 699,
-    image: '',
+    image: 'https://res.cloudinary.com/db5zkk8em/image/upload/v1652720417/phoneShop/iphone12_ntttmu.jpg',
   },
   {
     id: 3,
     name: 'Samsung Galaxy S20',
     desc: '6.5-inch display',
     price: 1099,
-    image: '',
+    image: 'https://res.cloudinary.com/db5zkk8em/image/upload/v1652720417/phoneShop/galaxyS_i4dzvz.png',
   },
 ]
 
